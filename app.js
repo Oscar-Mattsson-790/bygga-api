c4b332038554533dc734c6776c1f3a0cb6c56123
Oscar Mattsson <oscar.mattsson.790@gmail.com>
2023-03-16 17:26:25 +0700

    ongoing

--- app.js
+++ app.js
@@ -1,57 +1,50 @@
-import express, { response } from "express";
+import express from "express";
 const app = express(); // Detta skapar en express applikation
 const PORT = 8000;
 
-app.use(express.json()); // Tolka allt i body som JSON
+app.use(express.json()); // This parses all incoming requests as JSON'
 
-const insults = [
-  {
-    insult: "Neber hung posion on a fouler toad",
-    play: "Rickard III",
-  },
-  {
-    insults: "He thinks too much: such men are dangerous.",
-    play: "Julius Ceasar",
-  },
-];
-
-app.get("/api/insults", (request, response) => {
-  // response.send(JSON.stringify(insults));
-  response.json(insults); // Kör JSON.stringyfy automatiskt så det blir samma som ovan kod
+app.get("/", (request, response) => {
+  response.send('"Hello World!');
 });
 
-app.post("/api/insults", (request, response) => {
-  const insultObj = request.body;
+// We'll keep track of user accounts in this array
+const users = [];
 
-  if (insults.hasOwnProperty("insult") && insultObj.hasOwnProperty("play")) {
-    const { insult, play } = insultObj;
+// Login endpoint
+app.post("/api/login", (request, response) => {
+  const { username, password } = request.body;
+  const user = users.find((u) => u.username === username);
 
-    insult.push({ insult: insult, play: play });
-
-    const result = {
-      sucess: true,
-      insults: insults,
-    };
-
-    response.json(result);
-  } else {
-    const result = {
-      sucess: false,
-      insults: "Wrong data in body",
-    };
-
-    response.status(400).json(result);
+  if (!user) {
+    return response.json({ sucess: false });
   }
+
+  if (user.password !== password) {
+    return response.json({ sucess: false });
+  }
+
+  return response.json({ sucess: true });
 });
 
-app.get("/api/login", (request, response) => {
-  response.send("<h1>Välkommen!</h1>");
-});
+// Sugnup endpoint
+app.post("/api/signup", (request, response) => {
+  const { username, password, email } = request.body;
 
-app.get("/api/singup", (request, response) => {
-  response.send("<h1>Om</h1>");
+  // Check if username or email already exists
+  const usernameExists = users.some((u) => u.username === username);
+  const emailExists = users.some((u) => u.email === email);
+
+  if (usernameExists || emailExists) {
+    return response.json({ sucess: false, usernameExists, emailExists });
+  }
+
+  // Add new user to array
+  users.push({ username, password, email });
+
+  return response.json({ sucess: true, usernameExists, emailExists });
 });
 
 app.listen(PORT, () => {
-  console.log("Servern startad");
+  console.log("Server started on port", PORT);
 });
